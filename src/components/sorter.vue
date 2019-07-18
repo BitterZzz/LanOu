f1a650ba412a11b12e9af14ce136355d11d70582
4 <4>
2019-07-18 17:57:40 +0800

    分页组件封装

---
+++ src/components/sorter.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="pagetion">
+    <el-pagination
+      background
+      layout="prev, pager, next,slot, jumper"
+      :total="100"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrent"
+    >
+      <div class="page-show">
+        <span class="nowPage">4</span>
+        <i>/</i>
+        <span class="totalPage">10</span>
+      </div>
+    </el-pagination>
+  </div>
+</template>
+<script>
+export default {
+  name: "pagetion"
+};
+</script>
+<style lang="scss" scoped>
+.pagetion {
+  width: 100%;
+  text-align: center;
+  margin-top: 46px;
+  .page-show {
+    display: inline-block;
+    .nowPage {
+      color: #3a9ef4;
+      margin-right: -10px;
+    }
+    .totalPage {
+      color: #151515;
+      margin-left: -10px;
+    }
+    i {
+      display: inline-block;
+      margin-top: 2px;
+      padding: 0;
+    }
+  }
+}
+</style>
+
+
